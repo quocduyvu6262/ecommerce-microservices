aa99881b7aa0d611b8148ee8568b12c71161f146
quocduyvu6262 <quocduyvu6262@gmail.com>
2023-11-03 04:59:25 -0700

    Added Kafka.

---
+++ notification-service/src/main/java/com/microservices/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,20 @@
+package com.microservices.notificationservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+	@KafkaListener(topics = "notificationTopic")
+	public void handleNotification(OrderPlacedEvent orderPlacedEvent){
+		log.info("Received Notification for Order - {}", orderPlacedEvent.getOrderNumber());
+	}
+}
